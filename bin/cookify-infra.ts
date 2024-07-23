e8eff4d4ae75e41d0be06bc6a297bb4b99ec6c28
Tristan Phipson <tristan.phipson@bbd.co.za>
2024-07-23 09:55:31 +0200

    api gateway

--- bin/cookify-infra.ts
+++ bin/cookify-infra.ts
@@ -5,8 +5,8 @@ import { CookifyInfraStack } from '../lib/cookify-infra-stack';
 
 const app = new cdk.App();
 new CookifyInfraStack(app, 'CookifyInfraStack', {
-  apiCertArn: 'arn:aws:acm:eu-west-1:535492056399:certificate/b7059636-1708-41ac-8481-b0037fc49c7f',
-  apiDomain: 'api.cookify.phipson.co.za',
+  apiCertArn: 'arn:aws:acm:eu-west-1:535492056399:certificate/1f7241e0-21f5-4df7-90d4-7c8aca1cb0a0',
+  apiDomain: 'api.cookify.projects.bbdgrad.com',
   ec2KeyPairName: 'cookify-ec2-key',
   ghOrgName: 'BBD-Foodies',
   namingPrefix: 'cookify',
